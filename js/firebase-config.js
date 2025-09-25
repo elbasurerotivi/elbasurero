3f5dca39c7c0ff7c97f2a8bbc4561940a27a66b3
elbasurerotivi <elbasurerotivi@gmail.com>
2025-09-25 16:25:55 -0300

    Crear firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Configuración de Firebase (ejemplo, reemplaza con la tuya)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  databaseURL: "https://tu-proyecto-default-rtdb.firebaseio.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+// Inicializar Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
