25d060ae0e70321619df432d3f98a92f66f224f6
elbasurerotivi <elbasurerotivi@gmail.com>
2025-10-28 01:21:19 -0300

    premium

---
+++ js/videos-premium.js
@@ -0,0 +1,74 @@
+// js/videos.js
+export const videos = [
+  {
+    id: 'estar_en_el_horno',
+    title: 'Nada – Episodio 1 | Reacción con Luis Brandoni y la participación de Robert De Niro',
+    name: "Nada",
+    temporada: "1",
+    episodio: "1",
+    description: 'Primera impresión de Nada, una serie que combina humor, costumbrismo y reflexión sobre la vida, la comida y la soledad en Buenos Aires.\nLuis Brandoni encarna a un crítico gastronómico que debe aprender a vivir “sin nada” cuando su rutina se desmorona.\nY sí, aparece Robert De Niro, dándole un toque internacional inesperado.\n\nUna reacción llena de comentarios sobre estilo, tono y ese humor porteño tan particular.',
+    thumbnail: 'Imagenes/01 - Estar en el horno.jpg',
+    source: 'https://player.vimeo.com/video/1129400733?badge=0&amp',
+    type: 'iframe'
+  },
+  {
+    id: 'tarjeta_de_navidad',
+    title: 'Los Simuladores Episodio 1',
+    name: "Los Simuladores",
+    temporada: "1",
+    episodio: "1",
+    description: '🎬 Los Simuladores | Episodio 1 | "Tarjeta de navidad"',
+    thumbnail: 'Imagenes/simuladores_1.webp',
+    source: 'https://archive.org/embed/los-simuladores-2002/T1-01 Tarjeta de Navidad [NF-480p].mp4',
+    type: 'iframe'
+  },
+  {
+    id: 'un_proyecto_peligroso',
+    name: "El Encargado",
+    temporada: "1",
+    episodio: "1",
+    title: '01 Un Proyecto Peligroso🧹 El Encargado – Episodio 1 | Reacción completa con Guillermo Francella',
+    description: 'Francella vuelve, pero esta vez como nunca lo habías visto. En el primer episodio de El Encargado, conocemos a Eliseo, el portero de un edificio que parece tener más poder e información de la que debería.\nUna comedia con tintes oscuros, sarcasmo y crítica social al estilo argentino que tanto nos gusta.\nEn esta reacción exploramos ese primer vistazo al universo de Eliseo, su doble cara y todo el potencial que promete la serie.',
+    source: 'https://archive.org/details/01-un-proyecto-peligroso',
+    type: 'iframe'
+  },
+  {
+    id: 'una_accion_solidaria',
+    name: "El Encargado",
+    temporada: "1",
+    episodio: "2",
+    title: '02 Una Acción Solidaria🧹 El Encargado – Episodio 2 | Reacción completa con Guillermo Francella',
+    description: 'En este episodio, Eliseo profundiza en sus "acciones solidarias" que no son tan inocentes como parecen. Más intriga, más Francella en modo manipulador.',
+    source: 'https://archive.org/embed/02-una-accion-solidaria',
+    type: 'iframe'
+  },
+  {
+    id: 'un_oso_rojo',
+    title: 'Reacción A Un Oso Rojo Película Argentina HD',
+    description: 'Reacción A Un Oso Rojo Película Argentina HD',
+    source: 'https://archive.org/embed/reaccion-a-un-oso-rojo-pelicula-argentina-hd',
+    type: 'iframe'
+  },
+  {
+    id: 'simuladores_t1_01',
+    title: 'Los Simuladores Episodio 1',
+    description: '🎬 Los Simuladores | Episodio 1 | "Tarjeta de navidad"',
+    source: 'https://archive.org/embed/los-simuladores-2002/T1-01 Tarjeta de Navidad [NF-480p].mp4',
+    type: 'iframe'
+  },
+  {
+    id: 'drive_video',
+    title: 'Video desde Google Drive',
+    description: 'Un video exclusivo almacenado en Google Drive',
+    source: 'https://drive.google.com/file/d/TU_ID_AQUI/preview',
+    type: 'iframe'
+  },
+  {
+    id: 'dropbox_video',
+    title: 'Video desde Dropbox',
+    description: 'Un video exclusivo almacenado en Dropbox',
+    source: 'https://dl.dropboxusercontent.com/s/TU_ID_AQUI/mi-video.mp4?raw=1',
+    type: 'video'
+  }
+  // Agrega más videos aquí según necesites. Es escalable: solo edita este archivo.
+];
