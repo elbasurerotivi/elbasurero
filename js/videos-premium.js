d6888601cf976f3ca8faad133bc7a3beae9e1d1b
El Basurero <elbasurerotivi@gmail.com>
2025-11-03 10:12:29 -0300

    <dfs

--- js/videos-premium.js
+++ js/videos-premium.js
@@ -1,15 +1,29 @@
 // js/videos.js
 export const videosPremium = [
+
+  
   {
-    id: 'estar_en_el_horno',
-    name: "Nada",
+    id: 'un_turista_en_buenos_aires',
+    name: "El Encargado",
     temporada: "1",
-    episodio: "1",
-    title: '🍝 Estar en el horno',
-    title2:'Reacción con Luis Brandoni y la participación de Robert De Niro',
-    description: 'Primera impresión de Nada, una serie que combina humor, costumbrismo y reflexión sobre la vida, la comida y la soledad en Buenos Aires.\nLuis Brandoni encarna a un crítico gastronómico que debe aprender a vivir “sin nada” cuando su rutina se desmorona.\nY sí, aparece Robert De Niro, dándole un toque internacional inesperado.\n\nUna reacción llena de comentarios sobre estilo, tono y ese humor porteño tan particular.',
-    thumbnail: 'Imagenes/01 - Estar en el horno.jpg',
-    source: 'https://player.vimeo.com/video/1129400733?badge=0&amp',
+    episodio: "3",
+    title: '🧹  Un turista en Buenos Aires',
+    title2:'Reacción completa con Guillermo Francella',
+    description: 'Continuamos con El Encargado, y las cosas se ponen cada vez más interesantes. Este tercer episodio mantiene ese tono sarcástico y elegante que sólo Guillermo Francella puede sostener sin perder el encanto. Lo grabé el mismo día que el episodio anterior —sí, pleno Día de las Madres en Argentina— y fue imposible no seguir viendo. La serie tiene ese algo que te atrapa y no te deja parar. Una entrega exclusiva para Patreon, sin transmisión en vivo ni edición adicional, tal cual se vivió en el momento.',
+    thumbnail: 'Imagenes/3 - Un turista en Buenos Aires.png',
+    source: 'https://rumble.com/embed/v6yz9ey/?pub=4nmhh2',
+    type: 'iframe'
+  },
+  {
+    id: 'una_accion_solidaria',
+    name: "El Encargado",
+    temporada: "1",
+    episodio: "2",
+    title: '🧹  Una Acción Solidaria',
+    title2:'Reacción completa con Guillermo Francella',
+    description: 'En este episodio, Eliseo profundiza en sus "acciones solidarias" que no son tan inocentes como parecen. Más intriga, más Francella en modo manipulador.',
+    thumbnail: 'Imagenes/2 - Una accion solidaria.png',
+    source: 'https://rumble.com/embed/v6yywz0/?pub=4nmhh2',
     type: 'iframe'
   },
   {
@@ -24,34 +38,18 @@ export const videosPremium = [
     source: 'https://rumble.com/embed/v6yz8ey/?pub=4nmhh2',
     type: 'iframe'
   },
-
-  
   {
-    id: 'una_accion_solidaria',
-    name: "El Encargado",
+    id: 'estar_en_el_horno',
+    name: "Nada",
     temporada: "1",
-    episodio: "2",
-    title: '🧹  Una Acción Solidaria',
-    title2:'Reacción completa con Guillermo Francella',
-    description: 'En este episodio, Eliseo profundiza en sus "acciones solidarias" que no son tan inocentes como parecen. Más intriga, más Francella en modo manipulador.',
-    thumbnail: 'Imagenes/2 - Una accion solidaria.png',
-    source: 'https://rumble.com/embed/v6yywz0/?pub=4nmhh2',
+    episodio: "1",
+    title: '🍝 Estar en el horno',
+    title2:'Reacción con Luis Brandoni y la participación de Robert De Niro',
+    description: 'Primera impresión de Nada, una serie que combina humor, costumbrismo y reflexión sobre la vida, la comida y la soledad en Buenos Aires.\nLuis Brandoni encarna a un crítico gastronómico que debe aprender a vivir “sin nada” cuando su rutina se desmorona.\nY sí, aparece Robert De Niro, dándole un toque internacional inesperado.\n\nUna reacción llena de comentarios sobre estilo, tono y ese humor porteño tan particular.',
+    thumbnail: 'Imagenes/01 - Estar en el horno.jpg',
+    source: 'https://player.vimeo.com/video/1129400733?badge=0&amp',
     type: 'iframe'
   },
-
-  {
-    id: 'un_turista_en_buenos_aires',
-    name: "El Encargado",
-    temporada: "1",
-    episodio: "3",
-    title: '🧹  Un turista en Buenos Aires',
-    title2:'Reacción completa con Guillermo Francella',
-    description: 'Continuamos con El Encargado, y las cosas se ponen cada vez más interesantes. Este tercer episodio mantiene ese tono sarcástico y elegante que sólo Guillermo Francella puede sostener sin perder el encanto. Lo grabé el mismo día que el episodio anterior —sí, pleno Día de las Madres en Argentina— y fue imposible no seguir viendo. La serie tiene ese algo que te atrapa y no te deja parar. Una entrega exclusiva para Patreon, sin transmisión en vivo ni edición adicional, tal cual se vivió en el momento.',
-    thumbnail: 'Imagenes/3 - Un turista en Buenos Aires.png',
-    source: 'https://rumble.com/embed/v6yz9ey/?pub=4nmhh2',
-    type: 'iframe'
-  },
-  
   
   // Agrega más videos aquí según necesites. Es escalable: solo edita este archivo.
 ];
