f03d797cd816bef2113e50259a9e0de21553637b
El Basurero <elbasurerotivi@gmail.com>
2025-11-03 00:55:15 -0300

    dfas

--- js/videos-premium.js
+++ js/videos-premium.js
@@ -21,9 +21,10 @@ export const videosPremium = [
     title2: 'Reacción completa con Guillermo Francella',
     description: 'Francella vuelve, pero esta vez como nunca lo habías visto. En el primer episodio de El Encargado, conocemos a Eliseo, el portero de un edificio que parece tener más poder e información de la que debería.\nUna comedia con tintes oscuros, sarcasmo y crítica social al estilo argentino que tanto nos gusta.\nEn esta reacción exploramos ese primer vistazo al universo de Eliseo, su doble cara y todo el potencial que promete la serie.',
     thumbnail: 'Imagenes/2 - Una accion solidaria.png',
-    source: 'https://archive.org/embed/01-un-proyecto-peligroso',
+    source: 'https://player.vimeo.com/video/1133013432?h=4f3756800d',
     type: 'iframe'
   },
+
   {
     id: 'una_accion_solidaria',
     name: "El Encargado",
