0a0aef7e583240554d7e725d8f3593e54f015bf3
El Basurero <elbasurerotivi@gmail.com>
2025-11-10 15:43:41 -0300

    gd

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,9 @@
-// js/firebase-config.js
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { 
-  getAuth, 
-  onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { 
-  getDatabase, 
-  ref, 
-  push, 
-  onValue, 
-  set, 
-  remove, 
-  get, 
-  update 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getAuth,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase, ref, push, onValue, set, remove, get, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+// Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDqj18mvS9a4Ud3Y0voD5wolrhL3bSnhUw",
   authDomain: "elbasurero-af722.firebaseapp.com",
@@ -25,19 +14,10 @@ const firebaseConfig = {
   appId: "1:577473118837:web:6a6ab394c6cec3bff36e3c"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-export { 
-  auth, 
-  db, 
-  ref, 
-  push, 
-  onValue, 
-  set, 
-  remove, 
-  get, 
-  update, 
-  onAuthStateChanged 
-};
+// Exportar todo lo necesario
+export { auth, db, ref, push, onValue, set, remove, get, update, onAuthStateChanged };
