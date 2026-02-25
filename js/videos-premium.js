02abfea732f006a9c1fff8ed9438aef3a1f11736
El Basurero <elbasurerotivi@gmail.com>
2026-02-25 17:26:06 -0300

    rtghse

--- js/videos-premium.js
+++ js/videos-premium.js
@@ -75,47 +75,11 @@ export const videosPremium = [
   source: 'https://rumble.com/v75d8hy-nada-vuelve-a-ser-igual-reaccin-a-el-encargado-ep.-6-contenido-premium.html?mref=4nmhh2&mc=dzuzl',
   type: 'iframe'
 },
-   {
-  id: 'reaccion_a_pizza_birra_y_faso_peliculas',
-  name: 'Pizza, Birra y Faso',
-  categoria: 'peliculas',
-  temporada: '0',
-  episodio: '0',
-  title: 'Reacción a Pizza, Birra y Faso',
-  title2: 'Noche de descontrol',
-  description: 'Arrancamos con uno de esos clásicos del cine argentino que siempre escuché nombrar pero nunca había visto: Pizza, Birra y Faso. Lo vimos completito en vivo desde Twitch, en comunidad, y la experiencia fue una joya.\n\nLa película es cruda, rápida, barrial, directa al corazón y al estómago. Me sorprendió lo muchísimo que transmite con tan poco, y cómo te mete en ese mundo de supervivencia al límite. Tremendas actuaciones, tremenda vibra, y sí… me dejó con más ganas de seguir explorando cine argentino, como viene pasando últimamente en el canal.\n\nUna reacción llena de comentarios, risas, pausas y análisis improvisados, como siempre pasa cuando vemos pelis juntos.',
-  thumbnail: 'Imagenes/Picsart_25-11-12_10-58-59-894.jpg',
-  source: 'https://rumble.com/embed/v709nb6/?pub=4nmhh2',
-  type: 'iframe'
-},
 
-  {
-  id: 'reaccion_a_la_cienaga_peliculas',
-  name: 'La Ciénaga',
-  categoria: 'peliculas',
-  temporada: '0',
-  episodio: '0',
-  title: 'Reacción a La Ciénaga',
-  title2: 'Caos, calor y tensión',
-  description: 'Nos adentramos en La Ciénaga, una de las obras más celebradas del cine argentino contemporáneo. La vimos completa en Twitch y la experiencia fue intensa: ese calor sofocante, el caos familiar, la incomodidad constante… todo lo que hace única a la película de Lucrecia Martel estuvo presente desde el primer minuto.\n\nLa comunidad y yo reaccionamos a cada silencio, cada gesto extraño y cada escena cargada de tensión. Es una película que parece tranquila, pero te va apretando el pecho sin que te des cuenta. Perfecta para verla en grupo y comentar cada simbolismo, cada detalle escondido y cada momento incómodo.\n\nOtra joya del cine argentino compartida con ustedes.',
-  thumbnail: 'Imagenes/IWvEz.qR4e.jpg',
-  source: 'https://rumble.com/embed/v709nh6/?pub=4nmhh2',
-  type: 'iframe'
-},
 
-  {
-  id: 'reaccion_a_nazareno_cruz_y_el_lobo_peliculas',
-  name: 'Nazareno Cruz y el Lobo',
-  categoria: 'peliculas',
-  temporada: '0',
-  episodio: '0',
-  title: 'Reacción a Nazareno Cruz y el Lobo',
-  title2: 'Leyenda y humor involuntario',
-  description: 'Viajamos al pasado del cine argentino para reaccionar en Twitch a Nazareno Cruz y el Lobo, una película tan antigua que verla fue casi una aventura técnica. La calidad, el audio, los efectos… todo aportó a una comedia no intencional que terminó siendo deliciosamente divertida. La comunidad y yo no paramos de reír y sorprendernos.\n\nAun así, detrás de la risa hay algo importante: aunque la película no nos gustó como obra en sí, amamos verla juntos. Esa experiencia compartida no le quita mérito a su valor como clásico pionero del cine argentino, ni al lugar que ocupa en la historia del folclore visual del país.\n\nUna noche caótica, inesperadamente graciosa y muy especial.',
-  thumbnail: 'Imagenes/Picsart_25-11-13_20-15-45-582.jpg',
-  source: 'https://rumble.com/embed/v709nhy/?pub=4nmhh2',
-  type: 'iframe'
-},
+ 
+
+
 {
   id: 'visita_sorpresa_el_encargado',
   name: 'El Encargado',
@@ -274,19 +238,7 @@ export const videosPremium = [
 
   //peliculas//
 
-  {
-    id: 'el_patron',
-    categoria: 'peliculas',
-    name: 'El Patron',
-    temporada: '0',
-    episodio: '0',
-    title: '🥩 El Patrón: Radiografía de un Crimen',
-    title2: 'Película Cruda y Poderosa + Folklore Argentino',
-    description: 'Hoy en el canal nos aventamos una reacción intensa: “El Patrón: Radiografía de un Crimen”, una película argentina durísima basada en hechos reales. La historia de un carnicero atrapado en un sistema injusto, actuaciones brillantes y una crudeza que te deja pensando un buen rato. La verdad, tremenda película. Me sorprendió muchísimo.',
-    thumbnail: 'Imagenes/Reaccin-a-El-Patrn-Radiogra.jpg',
-    source: 'https://rumble.com/embed/v6zjt1o/?pub=4nmhh2',
-    type: 'iframe'
-  },
+  
 
   {
     id: 'millenium',
