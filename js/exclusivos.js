770b57516fa3ea2a4a6922724bd9bbca22d01773
El Basurero <elbasurerotivi@gmail.com>
2026-02-25 17:18:44 -0300

    gset

---
+++ js/exclusivos.js
@@ -0,0 +1,33 @@
+// js/exclusivos.js
+
+export const exclusivosData = {
+  key: 'exclusivos',
+  title: '🔥 Contenido Exclusivo',
+  background: 'Imagenes/ChatGPT Image 25 feb 2026, 04_16_42 p.m..png',
+  synopsis: 'Material especial disponible para toda la comunidad. Detrás de escenas, especiales y contenido único que no entra en otras categorías.',
+  year: '2026',
+  genre: 'Especiales • Comunidad • Extra',
+  cast: 'El Basurero y la comunidad'
+};
+
+export const videosExclusivos = [
+
+  {
+    id: 'especial_stream_01',
+    title: 'Especial detrás de escena',
+    description: 'Un vistazo completo al armado del stream, errores incluidos.',
+    thumbnail: 'Imagenes/thumb_exclusivo_1.jpg',
+    source: 'https://rumble.com/embed/xxxxx',
+    type: 'iframe'
+  },
+
+  {
+    id: 'short_exclusivo_02',
+    title: 'Momentos inéditos',
+    description: 'Clips que nunca llegaron a YouTube.',
+    thumbnail: 'Imagenes/thumb_exclusivo_2.jpg',
+    source: 'https://rumble.com/embed/yyyyy',
+    type: 'iframe'
+  }
+
+];
