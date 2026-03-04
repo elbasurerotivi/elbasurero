16583c67ced6c4791f32a6e4f298adc3e526a775
El Basurero <elbasurerotivi@gmail.com>
2026-03-04 14:08:21 -0300

    pñk

--- js/videos-premium.js
+++ js/videos-premium.js
@@ -235,6 +235,230 @@ export const videosPremium = [
     type: 'iframe'
   },
 
+    {
+  id: 'reaccion_a_nazareno_cruz_y_el_lobo_peliculas',
+  name: 'Nazareno Cruz y el Lobo',
+  categoria: 'peliculas',
+  temporada: '0',
+  episodio: '0',
+  title: 'Reacción a Nazareno Cruz y el Lobo',
+  title2: 'Leyenda y humor involuntario',
+  description: 'Viajamos al pasado del cine argentino para reaccionar en Twitch a Nazareno Cruz y el Lobo, una película tan antigua que verla fue casi una aventura técnica. La calidad, el audio, los efectos… todo aportó a una comedia no intencional que terminó siendo deliciosamente divertida. La comunidad y yo no paramos de reír y sorprendernos.\n\nAun así, detrás de la risa hay algo importante: aunque la película no nos gustó como obra en sí, amamos verla juntos. Esa experiencia compartida no le quita mérito a su valor como clásico pionero del cine argentino, ni al lugar que ocupa en la historia del folclore visual del país.\n\nUna noche caótica, inesperadamente graciosa y muy especial.',
+  thumbnail: 'Imagenes/Picsart_25-11-13_20-15-45-582.jpg',
+  source: 'https://rumble.com/embed/v709nhy/?pub=4nmhh2',
+  type: 'iframe'
+},
+ {
+  id: 'reaccion_a_la_cienaga_peliculas',
+  name: 'La Ciénaga',
+  categoria: 'peliculas',
+  temporada: '0',
+  episodio: '0',
+  title: 'Reacción a La Ciénaga',
+  title2: 'Caos, calor y tensión',
+  description: 'Nos adentramos en La Ciénaga, una de las obras más celebradas del cine argentino contemporáneo. La vimos completa en Twitch y la experiencia fue intensa: ese calor sofocante, el caos familiar, la incomodidad constante… todo lo que hace única a la película de Lucrecia Martel estuvo presente desde el primer minuto.\n\nLa comunidad y yo reaccionamos a cada silencio, cada gesto extraño y cada escena cargada de tensión. Es una película que parece tranquila, pero te va apretando el pecho sin que te des cuenta. Perfecta para verla en grupo y comentar cada simbolismo, cada detalle escondido y cada momento incómodo.\n\nOtra joya del cine argentino compartida con ustedes.',
+  thumbnail: 'Imagenes/IWvEz.qR4e.jpg',
+  source: 'https://rumble.com/embed/v709nh6/?pub=4nmhh2',
+  type: 'iframe'
+},
+
+   {
+  id: 'reaccion_a_pizza_birra_y_faso_peliculas',
+  name: 'Pizza, Birra y Faso',
+  categoria: 'peliculas',
+  temporada: '0',
+  episodio: '0',
+  title: 'Reacción a Pizza, Birra y Faso',
+  title2: 'Noche de descontrol',
+  description: 'Arrancamos con uno de esos clásicos del cine argentino que siempre escuché nombrar pero nunca había visto: Pizza, Birra y Faso. Lo vimos completito en vivo desde Twitch, en comunidad, y la experiencia fue una joya.\n\nLa película es cruda, rápida, barrial, directa al corazón y al estómago. Me sorprendió lo muchísimo que transmite con tan poco, y cómo te mete en ese mundo de supervivencia al límite. Tremendas actuaciones, tremenda vibra, y sí… me dejó con más ganas de seguir explorando cine argentino, como viene pasando últimamente en el canal.\n\nUna reacción llena de comentarios, risas, pausas y análisis improvisados, como siempre pasa cuando vemos pelis juntos.',
+  thumbnail: 'Imagenes/Picsart_25-11-12_10-58-59-894.jpg',
+  source: 'https://rumble.com/emb/v709nb6/?pub=4nmhh2',
+  type: 'iframe'
+},
+
+{
+    id: 'el_patron',
+    categoria: 'peliculas',
+    name: 'El Patron',
+    temporada: '0',
+    episodio: '0',
+    title: '🥩 El Patrón: Radiografía de un Crimen',
+    title2: 'Película Cruda y Poderosa + Folklore Argentino',
+    description: 'Hoy en el canal nos aventamos una reacción intensa: “El Patrón: Radiografía de un Crimen”, una película argentina durísima basada en hechos reales. La historia de un carnicero atrapado en un sistema injusto, actuaciones brillantes y una crudeza que te deja pensando un buen rato. La verdad, tremenda película. Me sorprendió muchísimo.',
+    thumbnail: 'Imagenes/Reaccin-a-El-Patrn-Radiogra.jpg',
+    source: 'https://rumble.com/embed/v6zjt1o/?pub=4nmhh2',
+    type: 'iframe'
+  },
+
+  ///Shinki No Kiojin///
+
+{  
+  id: 'shingeki_no_kyojin_t1_eps_21_25',
+  name: 'Shingeki No Kyojin',
+  categoria: 'shingeki_no_kyojin',
+  temporada: '1',
+  episodio: '21-25',
+  title: '🧭 Shingeki No Kyojin T1 Eps. 21–25',
+  title2: 'Final de Temporada 1',
+  description: 'Los primeros 6 episodios están en YouTube. Los ví y reaccioné a ellos pero nunca los subí completos a ninguna plataforma. Se pueden ver en una lista de reproducción en mi canal El Basurero. Aquí seguimos con la primera temporada de Shingeki no Kyojin, ahora con los episodios 21 al 25, y ¡QUÉ BARBARIDAD! Esta parte es una verdadera montaña rusa.',
+  thumbnail: 'Imagenes/Reaccin-a-Shingeki-no-Kyoji4.jpg',
+  source: 'https://rumble.com/embed/v6zk0xi/?pub=4nmhh2',
+  type: 'iframe'
+},
+{  
+  id: 'shingeki_no_kyojin_t1_eps_16_20',
+  name: 'Shingeki No Kyojin',
+  categoria: 'shingeki_no_kyojin',
+  temporada: '1',
+  episodio: '16-20',
+  title: '🧭 Shingeki No Kyojin T1 Eps. 16–20',
+  title2: 'Levi, la expedición y el Titán Femenino',
+  description: 'Estos episodios presentan el mundo real fuera de los muros: la Legión de Reconocimiento brilla, Levi demuestra por qué es una leyenda, y aparece uno de los misterios más importantes de toda la serie: el Titán Femenino.\nEl suspenso, las muertes inesperadas y la construcción del mundo hacen que este bloque sea imposible de ver sin comentar cada detalle.\nNota: mis reacciones completas a los episodios 1–6 están disponibles en YouTube, en la playlist de El Basurero.',
+  thumbnail: 'Imagenes/Shingeki-no-Kyojin-T1-Eps3..jpg',
+  source: 'https://rumble.com/embed/v6zjmk8/?pub=4nmhh2',
+  type: 'iframe'
+},
+
+{ 
+  id: 'shingeki_no_kyojin_t1_eps_12_15',
+  name: 'Shingeki No Kyojin',
+  categoria: 'shingeki_no_kyojin',
+  temporada: '1',
+  episodio: '12-15' ,
+  title: '💥 Shingeki No Kyojin T1 Eps. 12–15',
+  title2: 'La caída del Titán y el juicio de Eren',
+  description: 'Aquí la tensión se dispara: la lucha desesperada por sellar la brecha, el sacrificio del escuadrón y el momento en que Eren intenta controlar su poder. Después, el juicio donde el destino de Eren queda en manos del gobierno —una escena increíble por su intensidad y abuso de autoridad.\nUna mezcla de acción brutal, política interna y decisiones que cambian el rumbo de los personajes.\nRecordatorio: los episodios 1–6 pueden verse en mi canal de YouTube El Basurero, reacción completa en una playlist especial.',
+  thumbnail: 'Imagenes/Shingeki-no-Kyojin-T1-Eps.2.jpg',
+  source: 'https://rumble.com/embed/v6zjmj6/?pub=4nmhh2',
+  type: 'iframe'
+},
+{
+  id: 'shingeki_no_kyojin_t1_eps_7_11',
+  name: 'Shingeki No Kyojin',
+  categoria: 'shingeki_no_kyojin',
+  temporada: '1',
+  episodio: '7-11',
+  title: '⚔️ Shingeki No Kyojin T1 Eps. 7–11',
+  title2: 'El caos del Titán misterioso',
+  description: 'La historia da un salto enorme en estos episodios: Eren regresa… pero no como nadie esperaba. La aparición del Titán misterioso, las batallas dentro de Trost y el caos emocional del escuadrón convierten este bloque en uno de los más intensos de la primera temporada.\nEntre gritos, teorías y puro desconcierto, estas reacciones fueron una montaña rusa total.\nDato importante: mis reacciones a los episodios 1–6 están completas en mi canal de YouTube El Basurero, en una lista de reproducción exclusiva.',
+  thumbnail: 'Imagenes/Shingeki-no-Kyojin-T1-Eps..jpg',
+  source: 'https://rumble.com/embed/v6zjmho/?pub=4nmhh2',
+  type: 'iframe'
+},
+{
+  id: 'ya_no_te_amo_raul',
+  categoria: 'bloqueados_de_youtube',
+  name: '😂 Les Luthiers',
+  temporada: '0',
+  episodio: '1',
+  title: 'Ya no te amo, Raúl',
+  title2: 'No pude parar de reir',
+  description: 'Algunas piezas de humor simplemente no envejecen, y Ya no te amo, Raúl de Les Luthiers es una de ellas.\nUna joya de la ironía, la música y el absurdo más elegante, que me tuvo riendo de principio a fin.\nEntre el ingenio de las letras, los juegos de palabras y esa interpretación tan característica del grupo, fue imposible no rendirse ante el talento de estos genios.\nUna reacción llena de risas sinceras y admiración total por lo que hacen.',
+  thumbnail: 'Imagenes/NO-PUDE-PARAR-DE-RER-con-Y.jpg',
+  source: 'https://rumble.com/embed/v6zcjy0/?pub=4nmhh2',
+  type: 'iframe'
+},
+
+{
+    id: 'millenium',
+    categoria: 'peliculas',
+    name: 'Millenium',
+    temporada: '0',
+    episodio: '2',
+    title: '🔍Millennium',
+    title2: 'La chica que soñaba con un cerillo y un galón de gasolina + Peter Capusotto',
+    description: 'Una secuela que me agarró desprevenido: reaccionamos a La chica que soñaba con un cerillo y un galón de gasolina — la segunda parte de la saga sueca The Girl Who Played with Fire — donde todo se complica, la tensión se eleva y el caos aparece por todos lados.\nPara rematar, cerramos la sesión viendo un video de Peter Capusotto que nos hizo soltar carcajadas y pensar al mismo tiempo.\nUna experiencia de comunidad en Twitch plena de giros, comentarios sin filtro y descubrimientos… ¡no te la pierdas!',
+    thumbnail: 'Imagenes/Picsart_25-11-02_19-41-27-072.jpg',
+    source: 'https://rumble.com/embed/v6zbrnq/?pub=4nmhh2',
+    type: 'iframe'
+  },
+  {
+  id: 'millenium_2',
+  categoria: 'peliculas',
+  name: 'Millenium',
+  temporada: '0',
+  episodio: '1',
+  title: '🔍Millennium 2',
+  title2: 'Los hombres que no amaban a las mujeres (sueca)',
+  description: 'En este directo de Twitch vimos juntos Los hombres que no amaban a las mujeres, la versión sueca de la primera entrega de la saga Millennium — dirigida por Niels Arden Oplev y protagonizada por Noomi Rapace y Mikael Nyqvist.',
+  thumbnail: 'Imagenes/Picsart_25-10-24_19-36-11-553.jpg',
+  source: 'https://rumble.com/embed/v6zblns/?pub=4nmhh2',
+  type: 'iframe'
+},
+
+  {
+  id: 'encuentros_cercanos',
+  categoria: 'peliculas',
+  name: '👽 ENCUENTROS CERCANOS DEL TERCER TIPO',
+  temporada: '0',
+  episodio: '0',
+  title: 'ENCUENTROS CERCANOS DEL TERCER TIPO',
+  title2: 'Cine, misterio y nostalgia en comunidad',
+  description: 'Nos reunimos en Twitch para ver juntos Encuentros Cercanos del Tercer Tipo, la obra maestra de Steven Spielberg que mezcla ciencia ficción, asombro y humanidad como pocas películas lo han hecho.\nFue una experiencia increíble verla en comunidad: teorías, risas, debates sobre ovnis y momentos en los que todos quedamos con la piel de gallina.\nUna joya del cine setentero que sigue generando la misma fascinación hoy que el día en que se estrenó.\nUna reacción completa, sincera y llena de nostalgia.',
+  thumbnail: 'Imagenes/yNtyz.qR4e-small--REACCIN-A-ENCUENTROS-CERCA.jpg',
+  source: 'https://rumble.com/embed/v6zbr2o/?pub=4nmhh2',
+  type: 'iframe'
+},
+
+
+  
+    {
+    id: 'mi_obra_maestra',
+    categoria: 'peliculas',
+    name: "Mi Obra Maestra",
+    temporada: "0",
+    episodio: "0",
+    title: '🎨 Mi obra maestra',
+    title2:'Con Guillermo Francella y Luis Brandoni',
+    description: 'En esta sesión completa en directo vimos Mi obra maestra, una película argentina que mezcla humor, tensión del mercado del arte y la ambición entre dos amigos: un galerista de éxito algo manipulador y un pintor talentoso pero en decadencia. Después de la película cerramos con un video del canal “Te Lo Resumo Así Nomás” sobre el cine argentino, lo que nos dejó con muchas más ganas de explorar nuevas joyitas del cine de Argentina. Una combinación de reacción auténtica, conversación sincera y descubrimiento cultural… ¡como siempre!',
+    thumbnail: 'Imagenes/REACCIN-A-Mi-obra-maestra-.jpg',
+    source: 'https://rumble.com/embed/v6z9dmc/?pub=4nmhh2', 
+    type: 'iframe'
+  },
+
+  {
+    id: 'indiana_jones_2',
+    categoria: 'peliculas',
+    name: "Indiana Jones",
+    temporada: "0",
+    episodio: "2",
+    title: '💀 INDIANA JONES Y EL TEMPLO DE LA PERDICIÓN',
+    title2:'Sin video de Mariano… pero con Ice Cube',
+    description: 'Volvimos con la segunda entrega de la saga: Indiana Jones y el Templo de la Perdición. Una película más oscura, más intensa y con momentos que siguen sorprendiendo décadas después. Esta vez rompimos la tradición: no hubo video de Mariano Rodríguez sobre la peli, pero no podíamos cerrar sin él, así que terminamos viendo su video sobre La Guerra de los Mundos con Ice Cube. Entre risas, comentarios y reacciones genuinas, fue una sesión redonda.',
+    thumbnail: 'Imagenes/Picsart_25-10-21_14-04-12-948.jpg',
+    source: 'https://rumble.com/embed/v6z7l10/?pub=4nmhh2',
+    type: 'iframe'
+  },
+
+  {
+    id: 'indiana_jones_1',
+    categoria: 'peliculas',
+    name: "Indiana Jones",
+    temporada: "0",
+    episodio: "1",
+    title: '🧢 INDIANA JONES Y LOS CAZADORES DEL ARCA PERDIDA',
+    title2:'Increíble final + Mariano Rodríguez',
+    description: 'Nos lanzamos a revivir Indiana Jones y los Cazadores del Arca Perdida, la primera aventura del arqueólogo más icónico del cine. Entre risas, teorías y nostalgia pura, descubrimos por qué esta película sigue siendo una joya absoluta. Y para cerrar con broche de oro, vimos un video de Mariano Rodríguez en YouTube que nos dejó reflexionando y riendo al mismo tiempo. Un clásico, una gran charla y una reacción sin filtros, como siempre.',
+    thumbnail: 'Imagenes/Picsart_25-10-14_10-51-57-730.jpg',
+    source: 'https://rumble.com/embed/v6z7kx6/?pub=4nmhh2',
+    type: 'iframe'
+  },
+  
+  {
+    id: 'milo_j',
+    categoria: 'bloqueados_de_youtube',
+    name: "Milo J",
+    temporada: "",
+    episodio: "",
+    title: '🔥 MILO J',
+    title2:'Tres canciones que me hicieron cambiar de opinión',
+    description: 'Mi hijo Fabio me recomendó tres canciones de Milo J que, según él, merecían una oportunidad. Yo no soy fan del trap, pero este álbum tiene un toque folklórico que me tomó totalmente por sorpresa. Estas piezas no pueden verse en YouTube, así que esta reacción es contenido exclusivo para quienes disfrutan lo mejor de la música argentina, sin censura ni recortes. Lo confieso: no me gusta Milo J… pero estas tres canciones me volaron la cabeza.',
+    thumbnail: 'Imagenes/reaccion_milo_j.jpg',
+    source: 'https://rumble.com/embed/v6z6sxy/?pub=4nmhh2',
+    type: 'iframe'
+  },
+
+
   
   
   
