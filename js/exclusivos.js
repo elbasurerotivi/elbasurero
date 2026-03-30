a15234286ec8f1791b18f68b091c1a4fde139647
El Basurero <elbasurerotivi@gmail.com>
2026-03-30 03:26:44 -0600

    Update exclusivos.js

--- js/exclusivos.js
+++ js/exclusivos.js
@@ -977,14 +977,14 @@ export const videosExclusivos = [
 },
 
   {
-  id: 'directo_snk_t4_parte_4_eps_73_77_shingeki_no_kyojin',
+  id: 'directo_snk_t4_parte_4_eps_73_78_shingeki_no_kyojin',
   name: 'Shingeki No Kyojin',
   categoria: 'shingeki_no_kyojin',
   temporada: '4',
   episodio: '73-77',
-  title: '🎥 Directo — SNK T4 Parte 4 (Eps. 73–77)',
+  title: '🎥 Directo — SNK T4 Parte 4 (Eps. 73–78)',
   title2: 'Reacción completa en vivo',
-  description: 'Reacciones en vivo a Attack on Titan con la comunidad en Twitch.\n\nEn este directo vimos los episodios 73 al 77, donde todo sigue escalando y el panorama se vuelve cada vez más pesado.\n\nDe esos momentos donde ya no es solo intensidad… es peso.',
+  description: 'Reacciones en vivo a Attack on Titan con la comunidad en Twitch.\n\nEn este directo vimos los episodios 73 al 78, donde todo sigue escalando y el panorama se vuelve cada vez más pesado.\n\nDe esos momentos donde ya no es solo intensidad… es peso.',
   thumbnail: 'Imagenes/🎥 Directo — SNK T4 Parte 4 (Eps. 73–77).png',
   source: 'https://rumble.com/embed/v75kb42/?pub=4nmhh2',
   type: 'iframe'
