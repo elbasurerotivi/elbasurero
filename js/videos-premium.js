3536ea79c52edc2b20f678b178346faddc62108f
El Basurero <elbasurerotivi@gmail.com>
2026-04-15 01:38:49 -0600

    Update videos-premium.js

--- js/videos-premium.js
+++ js/videos-premium.js
@@ -40,7 +40,7 @@ export const categoriasData = {
             peliculas: {
     key: 'peliculas',
     name:'Películas',
-    background: 'Imagenes/ChatGPT Image 15 abr 2026, 01_16_17 a.m.',     // Fondo grande
+    background: 'Imagenes/ChatGPT Image 15 abr 2026, 01_16_17 a.m..png',     // Fondo grande
     cover: 'Imagenes/poster_peliculas.png',               // Carátula pequeña  
     title: 'Películas',
     synopsis: 'Este es un espacio dedicado especialmente a todas esas peliculas que vemos cada semana!!',
