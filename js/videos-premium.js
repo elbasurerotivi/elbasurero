eb3c0d417869de11db56ed7332b3befcc1e8863b
El Basurero <elbasurerotivi@gmail.com>
2026-08-10 02:52:47 -0600

    Update videos-premium.js

--- js/videos-premium.js
+++ js/videos-premium.js
@@ -4,7 +4,7 @@ export const categoriasData = {
             peliculas: {
     key: 'peliculas',
     name:'Películas',
-    background: 'Imagenes/Reacción Twitch, Metegol.png',     // Fondo grande
+    background: 'Imagenes/Títere enamorado de Medianeras.png',     // Fondo grande
     cover: 'Imagenes/poster_peliculas.png',               // Carátula pequeña  
     title: 'Películas',
     synopsis: 'Este es un espacio dedicado especialmente a todas esas peliculas que vemos cada semana!!',
